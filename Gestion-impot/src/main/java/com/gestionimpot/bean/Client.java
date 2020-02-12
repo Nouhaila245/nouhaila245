6d3cac20aeb92486b525960a9d372f2f89b8042a
fujitsu <fujitsu@DESKTOP-O5RQCBS>
2020-02-12 17:51:53 +0100

    hbubgugbh

--- Gestion-impot/src/main/java/com/gestionimpot/bean/Client.java
+++ Gestion-impot/src/main/java/com/gestionimpot/bean/Client.java
@@ -4,7 +4,7 @@ import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
-
+//nouhaila @ghassane
 @Entity
 public class Client {
 	@Id
