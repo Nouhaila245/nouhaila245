5177a3c92a43cd5e0880308e708de799c73247c9
ghassane <ghassane20a@gmail.com>
2020-02-13 12:10:45 +0100

    test

--- Gestion-impot/src/main/java/com/gestionimpot/bean/Client.java
+++ Gestion-impot/src/main/java/com/gestionimpot/bean/Client.java
@@ -4,7 +4,7 @@ import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
-
+//test
 @Entity
 public class Client {
 	@Id
